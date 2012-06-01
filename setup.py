f1ad29d15d65ac997ba4369f02534b5543e7b2d7
Oren Held <oren@onavo.com>
2012-06-01 22:39:17 +0300

    Added proper setup.py, renamed main module

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name = 'myip',
+      version = '0.1',
+      description = 'Tool for displaying IP',
+      author = 'Oren Held',
+      author_email = 'oren@held.org.il',
+      url = 'http://www.github.com/orenhe/myip',
+      packages = find_packages(),
+      license = 'MIT',
+      entry_points = {
+          'console_scripts': ['myip = myip.myip_cmd:main'],
+      }
+     )
